bb49b136a059a98483366c1b77fa5a08dc7d3495
Emre Guneyler <eguneys@gmail.com>
2023-10-06 00:44:18 +0300

    workers format es

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import { esbuildVersion } from 'vite'
 
 export default defineConfig({
   base: './',
@@ -9,6 +10,7 @@ export default defineConfig({
     topLevelAwait()
   ],
   worker: {
+    format: "es",
     plugins: [
       wasm(),
       topLevelAwait()
