53f8c7d637496f9e2cef3d794e511420468e3d75
Emre Guneyler <eguneys@gmail.com>
2023-10-06 16:41:28 +0300

    mate has tag

--- src/piworker.ts
+++ src/piworker.ts
@@ -1,7 +1,9 @@
 import type { FenMoves, FenPattern } from './pi.ts'
-import { play_tuples, pattern as pi_pattern } from 'pichess24'
+import { flip_colors, play_tuples, pattern as pi_pattern } from 'pichess24'
 
-type WorkerMsg = { id: string, 
+type WorkerMsg = { 
+  id: string, 
+  fen_flips?: string[],
   fen_moves?: FenMoves[], 
   fen_patterns?: FenPattern[] 
 }
@@ -9,13 +11,16 @@ type WorkerMsg = { id: string,
 onmessage = (e: MessageEvent<WorkerMsg>) => {
   let id = e.data.id
 
-  if (e.data.fen_moves) {
+  if (e.data.fen_flips) {
+
+    let result = e.data.fen_flips.map(fen => flip_colors(fen))
+    postMessage({ id, result })
+  } else if (e.data.fen_moves) {
     let result = e.data.fen_moves.map(({fen, moves}) => {
       return play_tuples(fen, moves) ?? 'undefined'
     })
     postMessage({ id, result })
-  }
-  if (e.data.fen_patterns) {
+  } else if (e.data.fen_patterns) {
     let result = e.data.fen_patterns.map(({fen, pattern}) => {
       return `${pi_pattern(fen, pattern)}`
     })
